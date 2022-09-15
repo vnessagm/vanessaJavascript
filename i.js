73b31c3f7eeb398083cda54af0e2be313f476ef6
Vanessa Gonçalves <98989640+vnessagm@users.noreply.github.com>
2022-09-15 18:02:01 -0300

    Create i.js

---
+++ i.js
@@ -0,0 +1,12 @@
+let first = 10
+console.log(first)
+let user = "Vanessa"
+console.log(Vanessa)
+let age = 25
+console.log(age)
+let sum = first + age
+console.log(sum)
+let main = "always";
+let side = "I was loved by him"
+while (main != side){
+    console.log("It's different")
